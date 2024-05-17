128e4ba34a3a799f80e87de3b128060c73c98115
milcho0604 <milcho0604@gmail.com>
2024-05-17 16:25:27 +0900

    메시지 입력 test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>MyText1</h1>
+    <MyText1 title="TEXT1" description="첫째 컴포넌트" color="#ffb"  v-on:send="showValue" />
+    <MyText1 title="TEXT2" color="#afa"  v-on:send="showValue" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyText1 from './MyText1.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  methods: {
+    showValue(value) {
+      alert(value);
+    }
+  },
+  components: { MyText1 }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+div#app { padding: 0 30px 30px 30px; margin: 30px auto; max-width: 400px;
+      border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; }
 </style>
---
+++ src/MyText1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :style="{ backgroundColor: color }">
+    <h2>{{ title }} - <small>{{ description }}</small></h2>
+    <input type="text" v-model="message" />
+    <button type="button" @click="$emit('send', message)"> 전달 </button> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyText1",
+  props: {
+    title: { type: String, required: true},
+    description: { type: String, default: "작업"},
+    color: { type: String, default: "#fff"}
+  },
+  data() {
+    return { text: "입력하세요."};
+  }
+}
+</script>
+
+<style scoped>
+  div { border: 1px solid gray; padding: 0 20px 20px 20px; margin-top: 10px; }
+  input { padding: 5px; margin-right: 5px; }
+  button { padding: 0.4em 1em; margin-right: 5px; }
+</style>
